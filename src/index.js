fa06589481fc9b93532565a843f52151d3d0fa6f
lutangar <johan.dufour@gmail.com>
2017-11-15 17:41:35 +0100

    fix expose formatToken

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { DEFAULT_LOCALE, CRYPTOCOMPARE_CURRENCIES } from './constants';
 import fetchPrices from './fetchPrices';
 import formatBalance from './formatBalance';
 import formatNumber from './formatNumber';
+import formatToken from './formatToken';
 import toWei from './toWei';
 
 export default {
